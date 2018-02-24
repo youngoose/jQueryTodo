47711abfa95158852c4f6f6a70bdf6ec147f98a7
Younghoon Jo (Eddy) <edwardevjo@gmail.com>
2018-02-24 16:25:19 -0500

    initial commit version1

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+// Check off Specific Todos by clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todos
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+});
